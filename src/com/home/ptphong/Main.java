54fe8391daf85b3ec6ba78adc13c31cb1604b281
Phong Pham <phamthephong83@gmail.com>
2018-09-10 21:06:18 +0700

    Complete Sample 04 & 06

--- src/com/home/ptphong/Main.java
+++ src/com/home/ptphong/Main.java
@@ -27,23 +27,30 @@ public class Main {
 
         /*
         // Sample 02
-        ArrayList<int[]> testValues = new ArrayList<>();
-        testValues.add(new int[] {1});
-        testValues.add(new int[] {1, 2});
-        testValues.add(new int[] {1, 2, 3});
-        testValues.add(new int[] {1, 2, 3, 4});
-        testValues.add(new int[] {3, 3, 4, 4});
-        testValues.add(new int[] {3, 2, 3, 4});
-        testValues.add(new int[] {4, 1, 2, 3});
-        testValues.add(new int[] {1, 1});
-        testValues.add(new int[] {});
+        ArrayList<int[]> testValues02 = new ArrayList<>();
+        testValues02.add(new int[] {1});
+        testValues02.add(new int[] {1, 2});
+        testValues02.add(new int[] {1, 2, 3});
+        testValues02.add(new int[] {1, 2, 3, 4});
+        testValues02.add(new int[] {3, 3, 4, 4});
+        testValues02.add(new int[] {3, 2, 3, 4});
+        testValues02.add(new int[] {4, 1, 2, 3});
+        testValues02.add(new int[] {1, 1});
+        testValues02.add(new int[] {});
 
-        for (int [] array: testValues
+        for (int [] array: testValues02
              ) {
             System.out.println("Result: " + sample_02(array));
         }
         */
 
+        /*
+        // Sample 03
+        char[] sample03 = sample_03(new char[] {'a', 'b', 'c'}, 1, 2);
+        System.out.println("Result:" + Arrays.toString(sample03));
+        */
+
+        /*
         // Sample 04
        int[] testValues04 = {1234, 12005, -1, 1000, 0, -12345};
 
@@ -51,6 +58,48 @@ public class Main {
              ) {
             System.out.println("Result: " + sample_04(n));
         }
+        */
+
+        // Sample 06
+        ArrayList<int[]> testValues06 = new ArrayList<>();
+        testValues06.add(new int[] {1, 8, 3, 7, 10, 2});
+        testValues06.add(new int[] {1, 5, 3, 1, 1, 1, 1, 1, 1});
+        testValues06.add(new int[] {2, 1, 1, 1, 2, 1, 7});
+        testValues06.add(new int[] {1, 2, 3});
+        testValues06.add(new int[] {3, 4, 5, 10});
+        testValues06.add(new int[] {1, 2, 10, 3, 4});
+
+        for (int[] a: testValues06
+             ) {
+            System.out.println("Result: " + sample_06(a));
+        }
+    }
+
+    public static int sample_06(int[]a)
+    {
+        int length = a.length;
+
+        if(length < 3)
+            return -1;
+
+        int poe = 1;
+        for(int i = 0 ; i < length - 1; i++)
+        {
+            int sumLeft = 0, sumRight= 0;
+
+            for(int x = 0 ; x < poe ; x++)
+                sumLeft+= a[x];
+
+            for(int y = poe + 1; y < length ; y++)
+                sumRight += a[y];
+
+            if(sumLeft == sumRight)
+                return poe;
+            else
+                poe++;
+        }
+
+        return -1;
     }
 
     public static int sample_04(int n)
@@ -61,6 +110,22 @@ public class Main {
             result = (result * 10) + n % 10;
             n = n / 10;
         }
+        return result;
+    }
+
+    public static char[] sample_03(char[]a, int start, int len)
+    {
+        if(a==null || start < 0 || len < 0 || start + len > a.length)
+            return null;
+
+        char [] result = new char[len];
+        int index = 0;
+
+        for(int i = 0 ; i < len ; i++)
+        {
+            result[index++] = a[start++];
+        }
+
         return result;
     }
 
